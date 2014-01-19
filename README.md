3d74d5887dacda243ecbfc974f44484609965279
Hugo Arregui <hugo.arregui@gmail.com>
2014-01-19 14:56:07 -0300

    some personal settings

---
+++ README.md
@@ -0,0 +1,43 @@
+dwm - dynamic window manager
+============================
+[dwm](http://dwm.suckless.org/) is an extremely fast, small, and dynamic window manager for X.
+
+Version
+=======
+[dwm 6.0](http://dl.suckless.org/dwm/dwm-6.0.tar.gz)
+
+Patches
+=======
+
+(All patches can be found in their own branch)
+
+1. [Pertag](http://dwm.suckless.org/patches/pertag) 
+2. hidden-bar: shows top bar when urgent flag is set & unify top bar for all tags
+3. mouseover-nofocus: Mouse over on client doesn't change focus
+4. [Pango](http://dwm.suckless.org/patches/pango)
+
+Keybindings
+===========
+
+    "[M]"  monocle                        : [Alt] +  m
+    "[]="  tile                           : [Alt] +  t
+    "><>"  no layout                      : [Alt] +  f
+
+    Invoke dmenu.sh (custom dmenu script) : [Alt]   + [p]
+    Launch terminal                       : [Shift] + [Alt] + [Enter]
+    Kill a window                         : [Shift] + [Alt] + [c]
+    Quit                                  : [Shift] + [Alt] + [q]
+
+Other settings
+==============
+
+- monocle is the default layout
+- namster is 3
+- terminal set as: urxvt (unicode-rxvt)
+
+Note
+====
+
+http://www.mail-archive.com/ratpoison-devel@nongnu.org/msg00561.html
+
+AWT_TOOLKIT=MToolkit bin/jmeter
--- config.def.h
+++ config.def.h
@@ -28,14 +28,14 @@ static const Rule rules[] = {
 
 /* layout(s) */
 static const float mfact      = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster      = 1;    /* number of clients in master area */
-static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
+static const int nmaster      = 3;    /* number of clients in master area */
+static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "[M]",      monocle },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
 };
 
 /* key definitions */
@@ -67,9 +67,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
